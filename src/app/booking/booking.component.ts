5d42d3f58ca9d6c5887290649bb79e3e7e357aba
pratikp8490 <61320574+pratikp8490@users.noreply.github.com>
2020-05-07 21:55:45 +0530

    Add files via upload

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output,EventEmitter } from '@angular/core';
 import { FormBuilder , FormGroup, FormControl, Validators } from '@angular/forms';
 import { HttpClient , HttpHeaders } from '@angular/common/http';
 import { ApiService } from '../api.service';
@@ -17,8 +17,18 @@ jsonapi:any;
 postData: any;
 json;
 data = [];
+myForm;
+demo:any;
+obj:any = [];
+update:any;
 constructor(private router:Router,private httpClient: HttpClient, private service:ApiService, private formBuilder: FormBuilder) { 
-    this.eventbooking = new FormGroup({
+   
+  // console.log('router data', this.router.getCurrentNavigation().extras.state);
+  // this.demo = this.router.getCurrentNavigation().extras.state;
+
+  
+  this.eventbooking = new FormGroup({
+      id : new FormControl(''),
       post_title : new FormControl(''),
       mo_no : new FormControl(''),
       email: new FormControl(''),
@@ -27,10 +37,21 @@ constructor(private router:Router,private httpClient: HttpClient, private servic
       post_content: new FormControl(''),
       address: new FormControl(''),
       action : new FormControl('wp_posts')
-    });
+    }); 
   }
 
   ngOnInit(): void {
+
+    this.service.teacherMessage$ .subscribe(message => {
+      console.log("this is message",message);
+      this.obj = message;
+      console.log("msg",this.obj)
+      console.log("msssg",this.obj.post_title);
+      // console.log('name',this.demo[0].name)
+      // this.obj = this.demo[0];
+      // console.log('this is object form data',this.obj)
+    })
+    
     this.eventbooking = this.formBuilder.group({
       post_title: ['', [Validators.required]],
       mo_no: ['',[Validators.required,Validators.pattern("^[0-9]{10}$")]],
@@ -43,6 +64,7 @@ constructor(private router:Router,private httpClient: HttpClient, private servic
       post_content: ['', Validators.required],
       address: ['', [Validators.required,Validators.maxLength(250), Validators.minLength(10)]],
   })
+    this.service.teacherMessage$
   }
   get f() { return this.eventbooking.controls; } 
   onSubmit() {
@@ -53,24 +75,40 @@ constructor(private router:Router,private httpClient: HttpClient, private servic
     }
 }
   eventbook(value) {
-    this.jsonapi = `http://localhost/wordpress/wp-json/custom-plugin/tickets`
-    const headers = new HttpHeaders().set('Content-Type','application/x-www-form-urlencoded');
-    var log_id = localStorage.getItem('ID');
-    console.log(log_id);
-    return this.httpClient.post(
-      this.jsonapi,
-      `&log_id=${log_id}&post_title=${value.post_title}&mo_no=${value.mo_no}&email=${value.email}&eve_date=${value.eve_date}
-      &eve_time=${value.eve_time}&post_content=${value.post_content}&address=${value.address}
-      &action=${'wp_posts'}`,
-      {headers , responseType: 'text'})
-      .subscribe(
+    
+     this.service.booking(value).subscribe(
       data => {
-        this.data.push(data);
+        this.data.push(value);
         console.log("POST Request is successful ",data);
+        value.id = data
+          console.log('this is prm id ====>', value.id);
+          // localStorage.setItem('prmid', JSON.stringify(value.id));
+          this.eventbooking.reset();
       },
       error  => {
         console.log("Error", error);   
-        }        
+        }         
     )   
+  }
+
+  updatedatanew(value){
+    alert("Are you sure Update data");
+    console.log("this is value",value);
+    let prmid = JSON.parse(localStorage.getItem('prmid'));
+    console.log("primary id",prmid);
+    this.update = `http://localhost/wordpress/wp-json/custom-plugin/update?&prmid =${prmid}`
+    console.log("ID",this.update)
+    
+    const headers = new HttpHeaders().set('Content-Type','application/x-www-form-urlencoded');
+    return this.httpClient.put( this.update,`&post_title=${value.post_title}&mo_no=${value.mo_no}
+    &email=${value.email}&eve_date=${value.eve_date}
+    &eve_time=${value.eve_time}&post_content=${value.post_content}&address=${value.address}`, 
+    {headers, responseType: 'text'}).subscribe((data) =>{
+      console.log("updated record", value);
+      data = value;
+      console.log("********",data)
+      value.id = data;
+      localStorage.removeItem("prmid")
+    })   
   }
 }

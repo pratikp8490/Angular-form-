714ed229e08deb16f764c10a11c6421a1558f9ca
pratikp8490 <61320574+pratikp8490@users.noreply.github.com>
2020-05-07 21:57:28 +0530

    Add files via upload

--- src/app/showbooking/showbooking.component.ts
+++ src/app/showbooking/showbooking.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import { state } from '@angular/animations';
+
 
 @Component({
   selector: 'app-showbooking',
@@ -7,21 +11,44 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   styleUrls: ['./showbooking.component.css']
 })
 export class ShowbookingComponent implements OnInit {
-booking:any = [];
-list:any = [];
-  constructor(private http: HttpClient) { }
-
-  ngOnInit(): void {
+booking: any = [];
+send_data:any;
+@Input() MSG : object ;
+data:any = {}
+item:any = [] ;
+obj:any;
+// @Output() send = new EventEmitter<object>();
+@Output() redirect:EventEmitter<any> = new EventEmitter();
+  constructor(private http: HttpClient, private router:Router,private service:ApiService) { 
+    this.showbooking()
   }
+  ngOnInit(): void {
+    
+  }
+
 showbooking() {
-    let id = localStorage.getItem('ID');
-    console.log("Id *************", id)
-  const headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-  return this.http.get
-    (`http://localhost/wordpress/wp-json/custom-plugin/event?log_id=${id}`,
-      { headers, responseType: 'text' }).subscribe((data)=>{
+  this.service.show().subscribe((data)=>{
         console.log('this is api response',data);
-        this.booking.push(data);
+        this.booking = data 
+        // this.booking.push(data);
       });
+}
+
+
+update(item){
+  console.log(" ********** item ********** ", item)
+  console.log("this is id",item.id)
+  localStorage.setItem('prmid', JSON.stringify(item.id))
+  this.service.sendMessage(item);
+}
+delete(item){
+  console.log("this is sdeleted data", item)
+  console.log("this is id",item.id)
+  // localStorage.setItem('dltprmid', JSON.stringify(item.id))
+  // this.item.splice(i,1);
+  // console.log("deleted data",this.item)
+  this.service.removeitem(item).subscribe((data)=>{
+    console.log("Record deleted successfully",data);
+  })
 }
 }

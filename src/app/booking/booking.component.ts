2331a73a4ca6e3d126852fb5ba12d525141fc8cc
pratikp8490 <61320574+pratikp8490@users.noreply.github.com>
2020-05-23 11:25:33 +0530

    Add files via upload

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -21,6 +21,10 @@ myForm;
 demo:any;
 obj:any = [];
 update:any;
+prmid:any;
+obj1:any;
+isShow :boolean = false;
+isButtonVisible:boolean;
 constructor(private router:Router,private httpClient: HttpClient, private service:ApiService, private formBuilder: FormBuilder) { 
    
   // console.log('router data', this.router.getCurrentNavigation().extras.state);
@@ -28,16 +32,15 @@ constructor(private router:Router,private httpClient: HttpClient, private servic
 
   
   this.eventbooking = new FormGroup({
-      id : new FormControl(''),
-      post_title : new FormControl(''),
-      mo_no : new FormControl(''),
-      email: new FormControl(''),
-      eve_date: new FormControl(''),
-      eve_time: new FormControl(''),
-      post_content: new FormControl(''),
-      address: new FormControl(''),
-      action : new FormControl('wp_posts')
-    }); 
+    id: new FormControl(''),
+    post_title: new FormControl(''),//name
+    to_ping: new FormControl(''),//mo
+    post_content: new FormControl(''),//email
+    post_date: new FormControl(''),//date
+    post_excerpt: new FormControl(''),//service
+    // address: new FormControl(''),//address
+    post_content_filtered:new FormControl(''),
+    action: new FormControl('wp_posts')}); 
   }
 
   ngOnInit(): void {
@@ -53,18 +56,25 @@ constructor(private router:Router,private httpClient: HttpClient, private servic
     })
     
     this.eventbooking = this.formBuilder.group({
-      post_title: ['', [Validators.required]],
-      mo_no: ['',[Validators.required,Validators.pattern("^[0-9]{10}$")]],
-      email: ['', Validators.compose([
-        Validators.required,Validators.email, 
+      post_title: ['', Validators.required],                                  //name
+      to_ping: ['', [Validators.required, Validators.pattern("^[0-9]{10}$")]],  //mobie no
+      post_content: ['', Validators.compose([
+        Validators.required, Validators.email,
         Validators.pattern('^[a-zA-Z0-9.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
-      ])],
-      eve_date: ['', Validators.required],
-      eve_time: ['', Validators.required],
-      post_content: ['', Validators.required],
-      address: ['', [Validators.required,Validators.maxLength(250), Validators.minLength(10)]],
+      ])],                                                                      //email
+      post_date: ['', Validators.required],                                     //date
+      post_excerpt: ['', Validators.required],                                  //service
+      post_content_filtered: ['', [Validators.required,
+      Validators.maxLength(100)]],
   })
-    this.service.teacherMessage$
+    this.service.hide$
+     .subscribe(
+       hide =>{
+         console.log('this is bool value', hide)
+         this.isShow = hide;
+         this.isButtonVisible = true;
+       }
+     ) 
   }
   get f() { return this.eventbooking.controls; } 
   onSubmit() {
@@ -75,40 +85,42 @@ constructor(private router:Router,private httpClient: HttpClient, private servic
     }
 }
   eventbook(value) {
-    
+
      this.service.booking(value).subscribe(
       data => {
         this.data.push(value);
-        console.log("POST Request is successful ",data);
-        value.id = data
+        this.obj1 = data
+        console.log("POST Request is successful ",this.obj1);
+        value.id = this.obj1
           console.log('this is prm id ====>', value.id);
           // localStorage.setItem('prmid', JSON.stringify(value.id));
           this.eventbooking.reset();
       },
-      error  => {
-        console.log("Error", error);   
+      errors  => {
+        console.log("Error", errors);   
         }         
     )   
   }
 
   updatedatanew(value){
     alert("Are you sure Update data");
-    console.log("this is value",value);
-    let prmid = JSON.parse(localStorage.getItem('prmid'));
-    console.log("primary id",prmid);
-    this.update = `http://localhost/wordpress/wp-json/custom-plugin/update?&prmid =${prmid}`
+    console.log("this is value",value.id);
+     this.prmid = JSON.parse(localStorage.getItem('prmid'));
+    console.log("primary id",this.prmid);
+   
+    this.update = `http://localhost/wordpress/wp-json/custom-plugin/update?ID=${this.prmid}`
     console.log("ID",this.update)
     
     const headers = new HttpHeaders().set('Content-Type','application/x-www-form-urlencoded');
-    return this.httpClient.put( this.update,`&post_title=${value.post_title}&mo_no=${value.mo_no}
-    &email=${value.email}&eve_date=${value.eve_date}
-    &eve_time=${value.eve_time}&post_content=${value.post_content}&address=${value.address}`, 
+    
+    return this.httpClient.put(this.update,`post_content=${value.post_content}
+    &to_ping=${value.to_ping}&post_title=${value.post_title}&post_date=${value.post_date}
+    &post_excerpt=${value.post_excerpt},&post_content_filtered=${value.post_content_filtered}`,
     {headers, responseType: 'text'}).subscribe((data) =>{
-      console.log("updated record", value);
-      data = value;
-      console.log("********",data)
-      value.id = data;
-      localStorage.removeItem("prmid")
+      console.log("this is new data",data)
+      this.isShow = false     
+      this.isButtonVisible = false;
+      this.eventbooking.reset();
     })   
   }
 }

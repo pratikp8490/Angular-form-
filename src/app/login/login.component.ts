2d2a38466f3ad4cfb4e118183b43a24fa83f4c42
pratikp8490 <61320574+pratikp8490@users.noreply.github.com>
2020-05-23 11:27:11 +0530

    Add files via upload

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -31,7 +31,8 @@ jsonapifile : any
         },
         
         error => {
-          console.error(" Error ", error)
+          console.
+          error(" Error ", error)
         })
   }
 }       
